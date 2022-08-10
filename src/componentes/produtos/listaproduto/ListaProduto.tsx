e12777767b0b138af40529ce3174e5674f6eba10
Rian <brianalmeida998@gmail.com>
2022-08-10 10:33:46 -0300

    Adicionando Segurança a Lista de Produtos

--- src/componentes/produtos/listaproduto/ListaProduto.tsx
+++ src/componentes/produtos/listaproduto/ListaProduto.tsx
@@ -1,48 +1,82 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Box, Card, CardActions, CardContent, Button, Typography } from "@mui/material";
 import './ListaProduto.css'
+import Produto from "../../../models/Produto";
+import useLocalStorage from "react-use-localstorage";
+import { buscar } from "../../../service/Service";
 
 function ListaProduto() {
 
+    const [posts, setPosts] = useState<Produto[]>([])
+
+    const [token, setToken] = useLocalStorage('token');
+
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (token == "") {
+            alert("Você precisa estar logado")
+            navigate("/login")
+        }
+    }, [token])
+
+    async function getPost() {
+        await buscar("/produtos", setPosts, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+
+        getPost()
+
+    }, [posts.length])
+
     return (
         <>
-            <Box m={2}>
-                <Card variant="outlined">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                            Produtos
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            Título
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            Texto do Produto
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            Categoria
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Box display="flex" justifyContent="center" mb={1.5}>
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" className="marginLeft" size="small" color="primary">
-                                        Atualizar
-                                    </Button>
+            {
+                posts.map(post => (
+                    <Box m={2} >
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Produtos
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {post.nome}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {post.descricao}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {post.categoria?.nome}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Box display="flex" justifyContent="center" mb={1.5}>
+                                    <Link to={`/formularioProduto/${post.id}`} className="text-decorator-none" >
+                                        <Box mx={1}>
+                                            <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                                                Atualizar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                    <Link to={`/deletarProduto/${post.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button variant="contained" size='small' color="secondary">
+                                                Deletar
+                                            </Button>
+                                        </Box>
+                                    </Link>
                                 </Box>
-                            </Link>
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" size="small" color="secondary">
-                                        Deletar
-                                    </Button>
-                                </Box>
-                            </Link>
-                        </Box>
-                    </CardActions>
-                </Card>
-            </Box>
+                            </CardActions>
+                        </Card>
+                    </Box>
+                ))
+            }
         </>
     );
 }
