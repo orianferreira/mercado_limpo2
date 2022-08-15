aeeaa11e5d80a9734aeaaf13d865a04df1b6e009
LuanPiauilino <luanpsilva2001@gmail.com>
2022-08-15 11:27:49 -0300

    Linkando ao botão do carrinho

--- src/componentes/produtos/listaproduto/ListaProduto.tsx
+++ src/componentes/produtos/listaproduto/ListaProduto.tsx
@@ -76,7 +76,7 @@ function ListaProduto() {
 
                         
                         <div className="card-button">
-                                <Link to={`/compraProduto/${produtos.id}`} className="text-decorator-none">
+                            <Link to={`/carrinho/${produtos.id}`} className="text-decorator-none">
                                     <Button >
                                         <svg className="svg-icon" viewBox="0 0 20 20">
                                             <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
@@ -161,43 +161,7 @@ function ListaProduto() {
     }
 
     return (
-        <>
-                    {produtos.map(produto => (
-                <Box m={2} display="flex" justifyContent="center" className='boxContainer'>
-                    <Card variant="outlined" className='cardContainer'>
-
-                        <div className='cardProduct'>
-                            <img src={produto.foto} alt="Img" />
-
-                            <div className='cardProductInfo'>
-                                <Typography variant="h5" component="h2">
-                                    {produto.nome}
-                                </Typography>
-
-                                <Typography variant="body2" component="p">
-                                    R$ {produto.preco}
-                                </Typography>
-
-                                <Typography variant="body2" component="p">
-                                    Quantidade Máx: {produto.estoque}
-                                </Typography>
-
-                            </div>
-                        </div>
-
-                        <Box display="flex" flexDirection="column" justifyContent="center" mb={1.5}>
-                            <Link to={`/carrinho/${produto.id}`} className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" size='small' color="secondary">
-                                        Comprar
-                                    </Button>
-                                </Box>
-                            </Link>
-                        </Box>
-
-                    </Card>
-                </Box>
-            ))}
+        <>        
          <Box className="page-produtos-button">    
                {buttonComponent}    
             </Box>
