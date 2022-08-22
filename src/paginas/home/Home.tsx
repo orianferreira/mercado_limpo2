8633164e4a4410a7e6535fd4872051a1e30f3bd5
marcelooo616 <marcelooo2014@hotmail.com>
2022-08-22 18:18:35 -0300

    fix - modificações css/ adicionando componentes categoria

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -9,50 +9,101 @@ import Carousel from '../../componentes/carousel/carousel.js';
 
 function Home() {
   return (
-    <>   
-      <Grid  container direction="row" alignItems="center" className='caixa' >
-        
+    <>
+      <Grid container direction="row" alignItems="center" className='caixa' >
+
         <Grid >
           <Box>
-          <Carousel/>
+            <Carousel />
           </Box>
         </Grid>
 
         <Grid alignItems="center" item xs={12} >
           <Box >
             <Box className='b'>
-            <Typography
-              variant="h3"
-              align='center'
-              className="titulo"
-            >
-              mercado limpo
-            </Typography>
-            <Typography
-              variant="h5"
-              gutterBottom
-              color="textPrimary"
-              component="h5"
-              align="center"
-              className='mensagem'
-            >
-              Energia limpa a um click!
-            </Typography>
+              <Typography
+                variant="h3"
+                align='center'
+                className="titulo"
+              >
+                mercado limpo
+              </Typography>
+              <Typography
+                variant="h5"
+                gutterBottom
+                color="textPrimary"
+                component="h5"
+                align="center"
+                className='mensagem'
+              >
+                Energia limpa a um click!
+              </Typography>
+
+
+
+            </Box>
+            <Box className='container-categorias'>
+            <h3>Categoria</h3>
+              <div className='card-categoria'>
+                
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/P8vk6kO.png" />
+                  <h6  >Solar </h6>
+                </div>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/0vYhvZn.png" />
+                  <h6 >eólica </h6>
+                  </div>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/avfI2Dp.png" />
+                  <h6 >oceânica </h6>
+                  </div>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/dnBGAEH.png" />
+                  <h6 >hídrica </h6>
+                  </div>
+               
+              </div>
+
+              
+            </Box>
+            <Box className='container-categorias'>
+            <h3 >Tipo</h3>
+              <div className='card-categoria'>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/RO20au2.png" />
+                  <h6  >Pessoal </h6>
+                </div>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/Jy2CXjC.png" />
+                  <h6 >Residencial </h6>
+                  </div>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/VirfCBA.png" />
+                  <h6 >Empresarial </h6>
+                  </div>
+                <div className='card-categria-solar'>
+                  <img className="img-categoria" src="https://i.imgur.com/iuFlwfT.png" />
+                  <h6 >Industrial </h6>
+                  </div>
+                <div className='titulo-categoria'>
+                  
+                   
+                </div>
+              </div>
+            </Box>
             
 
-           
-            </Box>
-          
             <Grid item xs={12} >
-                  <TabProduto />
+              <TabProduto />
             </Grid>
           </Box>
-          
-          
+
+
         </Grid>
       </Grid>
 
-      
+
     </>
   );
 }
